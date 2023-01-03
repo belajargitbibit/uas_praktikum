3e81ac7062fd9d7017d3ec296dd9c7e08d5e6269
Bibit Kukuh Wicaksono <bibitkukuh00@gmail.com>
2023-01-03 20:01:35 +0700

    soal 10

--- uas-prak.php
+++ uas-prak.php
@@ -4,14 +4,16 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Nomor 9</title>
+    <title>Nomor 10</title>
 </head>
 <body>
 <?php
 $a = 50;
 $b = 10;
-if ($a > $b) {
+if ($a == $b) {
     echo "Benar<br>";
+} else {
+    echo "Salah<br>";
 }
 ?>
 </body>

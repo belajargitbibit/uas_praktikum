fffaec1dc34e155b72453bc75982f681ca2cfa4d
Bibit Kukuh Wicaksono <bibitkukuh00@gmail.com>
2023-01-03 20:33:17 +0700

    soal 12

--- uas-prak.php
+++ uas-prak.php
@@ -1,21 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Nomor 11</title>
-</head>
-<body>
-    <table border="1" cellpadding="10" cellspacing="0">
-        <?php
-        for ($b=1; $b<=10; $b++){
-            echo "<tr>";
-            for ($k=1; $k<=5; $k++){
-                echo "<td> $b,$k </td>";
-            }
-            echo "<tr>";
-        }
-        ?>
-</body>
-</html>
+<?php
+$i = 1;
+while ($i < 6) {
+    echo $i;
+    $i++;
+}
+?>

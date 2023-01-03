b96fb6caf935fcdc13b799d123b3e4130728f90a
Bibit Kukuh Wicaksono <bibitkukuh00@gmail.com>
2023-01-03 20:22:15 +0700

    soal 11

--- uas-prak.php
+++ uas-prak.php
@@ -1,7 +1,21 @@
-<?php 
-$colors = array("red","green","blue","yellow"); 
- 
-foreach ($colors as $x) {
-  echo $x;
-}
-?> 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Nomor 11</title>
+</head>
+<body>
+    <table border="1" cellpadding="10" cellspacing="0">
+        <?php
+        for ($b=1; $b<=10; $b++){
+            echo "<tr>";
+            for ($k=1; $k<=5; $k++){
+                echo "<td> $b,$k </td>";
+            }
+            echo "<tr>";
+        }
+        ?>
+</body>
+</html>
